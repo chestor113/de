6c360a8e3f46c04e722a6dffc0f9da7a6e58145f
Evg <chestor113@gmail.com>
2026-04-14 17:55:22 +0000

    commit

--- jobs/test_job.py
+++ jobs/test_job.py
@@ -1,8 +1,7 @@
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import col, from_json
 from pyspark.sql.types import StructType, StructField, StringType
-
-
+from pyspark.sql.functions import sha2, current_timestamp, lit, concat_ws
 
 def main():
     spark = SparkSession.builder \
@@ -17,11 +16,20 @@ def main():
 
     spark.sparkContext.setLogLevel("ERROR")
 
-    df = spark.read.parquet("s3a://datalake/raw/topic1/")
+    df = spark.read.parquet("s3a://datalake/raw/topic1/year=2026/")
 
     schema = StructType([
-    StructField("gender", StringType(), True),
-    StructField("email", StringType(), True)
+        StructField("gender", StringType(), True),
+        StructField("email", StringType(), True),
+        StructField("name", StructType([
+            StructField("first", StringType(), True),
+            StructField("last", StringType(), True)
+        ]), True),
+        StructField(
+            "login", StructType([
+                StructField("uuid", StringType(), True)
+            ]), True
+        )
     ])
  
     df2 = df.withColumn(
@@ -31,6 +39,70 @@ def main():
 
     df2.select("payload_json").show(5, truncate=False)
     df2.printSchema()
+
+    df3 = df2.select(
+        col("payload_json.login.uuid").alias("uuid"),
+        col("payload_json.name.first").alias("first_name"),
+        col("payload_json.name.last").alias("last_name"),
+        col("payload_json.gender").alias("gender"),
+        col("payload_json.email").alias("email")
+    )
+    df_hub = df3.withColumn(
+        "user_hk",
+        sha2(col("uuid"), 256)
+    )
+    
+    df3.show(5, truncate=False)
+    df3.printSchema()
+
+    df_hub = df3.select(
+        col("uuid").alias("user_bk"),
+        sha2(col("uuid"), 256).alias("user_hk")
+    )
+
+    df_hub = df_hub.withColumn(
+        "load_dts", current_timestamp()
+    ).withColumn(
+        "record_source", lit("randomuser_api")
+    )
+
+    df_hub.select("*").show(5, truncate=False)
+
+
+    df_sat = df3.withColumn(
+        "user_hk",
+        sha2(col("uuid"), 256)
+    )   
+
+    df_sat = df_sat.withColumn(
+        "hashdiff",
+        sha2(
+            concat_ws(
+                "|",
+                col("gender"),
+                col("first_name"),
+                col("last_name"),
+                col("email")
+            ),
+            256
+        )
+    )
+    df_sat = df_sat.withColumn(
+        "load_dts",
+        current_timestamp()
+    )
+    df_sat = df_sat.select(
+        "user_hk",
+        "load_dts",
+        "hashdiff",
+        "gender",
+        "first_name",
+        "last_name",
+        "email"
+    )
+
+    df_sat.show(5, False)
+    df_sat.printSchema()
 
 if __name__ == "__main__":
     main()
